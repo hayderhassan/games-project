f2492b025d5057198073ddd8c7f6be4514565420
hayderhassan <hayderh@gmail.com>
2017-03-20 04:16:23 +0000

    Update main.py

--- main.py
+++ main.py
@@ -24,11 +24,13 @@ class Game:
         self.running = True
         self.font_name = pg.font.match_font(FONT_NAME)
         self.load_data()
-        #self.back_to_home = False
-        #self.home_screen = False
-        #self.game_over_screen = False
+        self.back_to_home = False
+        self.home_screen = False
+        self.game_over_screen = False
         self.score = 0
-        #self.lives = 3
+        self.lives = 3
+
+    #    self.current_level_no = 0
 
     def load_data(self):
         #define file directory as directory of main.py
@@ -40,8 +42,8 @@ class Game:
 
         # Load buttons
         self.buttons = Spritesheet(path.join(img_dir, BUTTON_IMAGES))
-        #self.yellowButton1 = self.buttons.get_image(0, 188, 190, 49, 1)
-        #self.yellowButton1.set_colorkey(BLACK)
+        self.yellowButton1 = self.buttons.get_image(YELLOW_BUTTON1, 1)
+        self.yellowButton1.set_colorkey(BLACK)
 
         # Load fonts
         font_dir = path.join(self.dir, "fonts")
@@ -51,27 +53,55 @@ class Game:
         self.sound_dir = path.join(self.dir, "sound")
         #self.an_example_sound = pg.mixer.Sound(path.join(self.sound_dir, "sound_example.wav"))
 
+        # Pause screen
+        self.dim_screen = pg.Surface(self.screen.get_size()).convert_alpha()
+        self.dim_screen.fill((0, 0, 0, 180))
+
     def new(self):
         # start a new game
         print("new()")
+        self.paused = False
+        self.back_to_home = False
+        self.game_over_screen = False
         self.all_sprites = pg.sprite.LayeredUpdates()
         self.platforms = pg.sprite.Group()
         self.player = Player(self)
         self.all_sprites.add(self.player)
 
-        self.current_level = Level_01(g, self.player, self.all_sprites)
+        # Create all the levels
+#        self.level_list = []
+#        self.level_list.append((Level_1(g, self.player, self.all_sprites)))
+#        self.level_list.append((Level_2(g, self.player, self.all_sprites)))
+
+        # Set the current level
+#        self.current_level_no = 1
+#        self.current_level = self.level_list[self.current_level_no]
+#        print("current_level_no = " + str(self.current_level_no))
+#        print("level_list = " + str(self.level_list))
 
 
+
+        self.current_level = Level_1(g, self.player, self.all_sprites)
+
+     #   self.current_level_no += 1
+    #    self.level = "Level_" + str(self.current_level_no)
+     #   print(self.level)
+     #   self.current_level = self.level(g, self.player, self.all_sprites)
+     #   print(self.current_level)
         self.run()
 
     def run(self):
-        #print("run()")
+        print("run()")
         # Game Loop
         self.playing = True
+
         while self.playing:
             self.clock.tick(FPS)
             self.events()
-            self.update()
+            if not self.paused:
+                self.update()
+                #print("run() total lives: " + str(self.lives))
+                print("current world shift: " + str(self.current_level.world_shift))
             self.draw()
 
     def update(self):
@@ -126,28 +156,48 @@ class Game:
                 if sprite.rect.bottom < 0:
                     sprite.kill()
         if len(self.platforms) == 0:
-            #self.lives -= 1
+            self.lives -= 1
             self.playing = False
 
         # If the player gets near the right side, shift the world left (-x)
-        if self.player.pos.x > 400 and abs(self.player.vel.x) > 0:
-            shift = self.player.pos.x - 400
-            self.player.pos.x = 400
+        if self.player.pos.x > 500 and abs(self.player.vel.x) > 0:
+            shift = self.player.pos.x - 500
+            self.player.pos.x = 500
             self.current_level.shift_world(-shift)
 
 
         # If the player gets near the left side, shift the world right (+x)
-        if self.player.pos.x <= 120 and abs(self.player.vel.x) > 0:
-            shift = 120 - self.player.pos.x
-            self.player.pos.x = 120
+        if self.player.pos.x <= 300 and self.player.vel.x < 0:
+            shift = 300 - self.player.pos.x
+            self.player.pos.x = 300
             self.current_level.shift_world(shift)
 
-#        self.current_level.update()
+        if 5 == 5:
+            # If the player gets to the end of the level, go to the next level
+            current_position = self.player.rect.x + self.current_level.world_shift
+            print("Current position: " + str(current_position))
+            if current_position < self.current_level.level_limit:
+                self.player.rect.x = 120
+                self.current_level = Level_2(g, self.player, self.all_sprites)
+#                if self.current_level_no < len(self.level_list) - 1:
+#                    self.current_level_no += 1
+#                    self.current_level = self.level_list[self.current_level_no]
+#                    #self.player.level = self.current_level
 
     def events(self):
        # print("events()")
         # Game Loop - events
         for event in pg.event.get():
+
+            pos = pg.mouse.get_pos()
+            x = pos[0]
+            y = pos[1]
+
+            continue_button = (x >= 300 and x <= 490) and (y >= 220 and y <= 270)
+            restart_button = (x >= 300 and x <= 490) and (y >= 280 and y <= 330)
+            home_button = (x >= 300 and x <= 490) and (y >= 340 and y <= 390)
+            exit_button = (x >= 300 and x <= 490) and (y >= 400 and y <= 450)
+
             # check for closing window
             if event.type == pg.QUIT:
                 #if self.playing:
@@ -157,6 +207,7 @@ class Game:
             # Check for closing window
             if event.type == pg.QUIT:
                 if self.playing:
+                    self.lives = 0
                     self.playing = False
                 self.running = False
 
@@ -168,50 +219,160 @@ class Game:
             if event.type == pg.KEYDOWN:
                 if event.key == pg.K_UP:
                     self.player.jump()
+
+                # check if game is paused
+                if event.key == pg.K_p:
+                    self.paused = not self.paused
+
+                # check if "ESC" has been pressed to quit
                 if event.key == pg.K_ESCAPE:
                     print("Esc")
+                    if self.playing:
+                        self.lives = 0
+                        self.playing = False
                     self.running = False
+
+            if event.type == pg.MOUSEBUTTONDOWN and self.paused:
+                print(str(x) + ", " + str(y))
+                if continue_button:
+                    self.paused = False
+                if restart_button:
+                    self.lives = 3
+                    self.score = 0
                     self.playing = False
+                if home_button:
+                    self.lives = 0
+                    self.score = 0
+                    self.playing = False
+                    self.back_to_home = True
+                if exit_button:
+                    self.lives = 0
+                    self.playing = False
+                    self.running = False
 
     def draw(self):
        # print("draw()")
         # Game Loop - draw
 
         self.current_level.draw(self.screen)
-
+#        print("Level " + str(self.current_level_no))
         #self.screen.fill(BG_COLOUR)
         #self.current_level.all_sprites.draw(self.screen)
         #draw sprites onto screen:
  #       self.all_sprites.draw(self.screen)
 
+        self.draw_text("Score: " + str(self.score), 22, BLACK, (WIDTH / 4), 15)
+        self.draw_text("Lives: " + str(self.lives), 22, BLACK, (WIDTH * (3 / 4)), 15)
+
+        if self.paused:
+            self.screen.blit(self.dim_screen, (0, 0))
+            self.draw_text("Paused", 40, WHITE, WIDTH / 2, HEIGHT / 4)
+            self.draw_button("Continue", 300, (HEIGHT * (4 / 5)) - 180)
+            self.draw_button("Restart", 300, (HEIGHT * (4 / 5)) - 120)
+            self.draw_button("Home", 300, (HEIGHT * (4 / 5)) - 60)
+            self.draw_button("Exit", 300, ((HEIGHT * (4 / 5))))
+
         # *after* drawing everything, flip the display - nothing else should go below this line
         pg.display.flip()
+
+    def wait_for_user(self):
+        # wait for user to make choice
+
+        waiting = True
+        while waiting:
+            self.clock.tick(FPS)
+
+            pos = pg.mouse.get_pos()
+            x = pos[0]
+            y = pos[1]
+
+            play_again_button = (x >= 300 and x <= 488) and (y >= 280 and y <= 330)
+            play_button = (x >= 300 and x <= 488) and (y >= 340 and y <= 390)
+            home_button = (x >= 300 and x <= 488) and (y >= 340 and y <= 390)
+            exit_button = (x >= 300 and x <= 488) and (y >= 400 and y <= 450)
+
+            for event in pg.event.get():
+                if event.type == pg.QUIT:
+                    self.lives = 0
+                    waiting = False
+                    self.running = False
+                if event.type == pg.KEYUP:
+                    if event.key == pg.K_ESCAPE:
+                        self.lives = 0
+                        waiting = False
+                        self.running = False
+
+                # Game over screen
+                if event.type == pg.MOUSEBUTTONDOWN and self.game_over_screen:
+                    print(str(x) + ", " + str(y))
+                    if play_again_button:
+                        self.lives = 3
+                        self.score = 0
+                        self.game_over_screen = False
+                        waiting = False
+                        self.playing = True
+
+                    if home_button:
+                        self.game_over_screen = False
+                        waiting = False
+                        self.back_to_home = True
+
+                    if exit_button:
+                        self.lives = 0
+                        self.game_over_screen = False
+                        waiting = False
+                        self.running = False
+
+                # Home screen
+                if event.type == pg.MOUSEBUTTONDOWN and self.home_screen:
+                    print(str(x) + ", " + str(y))
+                    if play_button:
+                        waiting = False
+                    if exit_button:
+                        self.lives = 0
+                        waiting = False
+                        self.running = False
 
     def show_start_screen(self):
         # game start screen
         print("show start screen()")
+
+        self.home_screen = True
+        self.score = 0
+        self.lives = 3
+
         self.screen.fill(BG_COLOUR)
         self.draw_text(TITLE, 30, BLACK, 400, 100)
         self.draw_text("Use the arrow keys to move", 20, BLACK, 400, 200)
-        self.draw_button("Start", 300, 340)
+        self.draw_button("Play", 300, 340)
         self.draw_button("Exit", 300, 400)
 
-        # *after* drawing everything, flip the display - nothing else should go below this line
+        # *after* drawing everything, flip the display - no other drawings/text should go below this line
         pg.display.flip()
+
+        self.wait_for_user()
+        self.home_screen = False
 
     def show_go_screen(self):
         # game over/continue
         print("show game over screen()")
-        g.events()
-        self.screen.fill(BG_COLOUR)
 
-        self.draw_text("Game Over", 30, BLACK, 400, 100)
-        self.draw_text("Score: " + str(self.score), 20, BLACK, 400, 200)
+        if not self.running:
+            return
+
+        self.game_over_screen = True
+
+        self.screen.fill(BG_COLOUR)
+        self.draw_text("GAME OVER", 50, WHITE, WIDTH / 2, (HEIGHT / 4) - 40)
+        self.draw_text("Score: " + str(self.score), 22, WHITE, WIDTH / 2, (HEIGHT / 3) + 50)
+
         self.draw_button("Play Again", 300, 280)
-        self.draw_button("Menu", 300, 340)
+        self.draw_button("Home", 300, 340)
         self.draw_button("Exit", 300, 400)
 
         pg.display.flip()
+
+        self.wait_for_user()
 
     def draw_text(self, text, size, colour, x, y):
         font = pg.font.Font(self.title_font, size)
@@ -225,15 +386,18 @@ class Game:
         self.draw_text(text, 22, BLACK, x + 90, y + 10)
 
 print("start")
+
 g = Game()
+
 print("finish initialising game and show start screen")
-#while g.running:
-    #g.show_start_screen()
-    #g.show_go_screen()
-#    g.draw()
+g.show_start_screen()
 
 while g.running:
-    g.new()
-#    g.show_go_screen()
+    while g.lives > 0:
+        g.new()
+    if not g.back_to_home:
+        g.show_go_screen()
+    else:
+        g.show_start_screen()
 
 pg.quit()
